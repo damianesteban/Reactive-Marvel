93c99cac2fb2f6a687dba102e3be203e693e2a62
Damian Esteban <damian.esteban@gmail.com>
2016-06-01 16:28:05 -0400

    adds Comic model

---
+++ ReactiveMarvel/Comic.swift
@@ -0,0 +1,41 @@
+//
+//  Comic.swift
+//  ReactiveMarvel
+//
+//  Created by Damian Esteban on 6/1/16.
+//  Copyright © 2016 Damian Esteban. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+struct Comic: JSONAbleType {
+    let title: String
+    let issueNumber: Float
+    let description: String
+    let format: String
+    
+    let seriesName: String
+    
+    let imageURLString: String
+    
+    static func fromJSON(json: JSON) -> Comic {
+        let title = json["title"].stringValue
+        let issueNumber = json["issueNumber"].floatValue
+        let description = json["description"].stringValue
+        let format = json["format"].stringValue
+        let seriesName = json["series"]["name"].stringValue
+        let imagePath = json["thumbnail"]["path"].stringValue
+        let imageExtension = json["thumbnail"]["extension"].stringValue
+        let imageURLString = "\(imagePath).\(imageExtension)"
+        return Comic(title: title, issueNumber: issueNumber, description: description, format: format,
+                     seriesName: seriesName, imageURLString: imageURLString)
+    }
+    
+    static func arrayFromJSON(object: AnyObject) -> [Comic] {
+        let json = JSON(object)
+        return json["data"]["results"].arrayValue.map {
+            return Comic.fromJSON($0)
+        }
+    }
+}
